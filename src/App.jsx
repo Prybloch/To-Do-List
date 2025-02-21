c89c2191b2c45636dafdd79be7427d38c81bc2fe
Prybloch <prybloch@gmail.com>
2025-02-21 14:12:49 -0300

    Incluindo deletar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,61 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from "react";
 
-
 function App() {
- const [tasks, setTasks] = useState([
-  {
-    id: 1,
-    title: "Estudar programação",
-    description: "Estudar react para se tornar uma desenvolvedora.",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: "Estudar inglês",
-    description: "Estudar react para se tornar uma desenvolvedora.",
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    title: "Estudar japonês",
-    description: "Estudar japonês para morar no Japão um dia.",
-    isCompleted: false,
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar react para se tornar uma desenvolvedora.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar react para se tornar uma desenvolvedora.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar japonês",
+      description: "Estudar japonês para morar no Japão um dia.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
   }
- ]);
- return (
-  <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-    <div className="w-[500px]">
-    <h1 className="text-3xl text-slate-100 font-bold text-center"> Gerenciador de Tarefas</h1>
-    <AddTask />
-    <Tasks tasks={tasks} />
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task => task.id !== taskId));
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          {" "}
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks 
+        tasks={tasks} 
+        onTaskClick={onTaskClick}
+        onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
     </div>
-  </div>
- );
+  );
 }
 
 export default App;
